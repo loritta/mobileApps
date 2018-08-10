f1ee9493c236d482318c4ab5d41a40c3676aeb55
STUDENT.JAC.Lara <lara@lara.com>
2018-08-10 13:26:55 -0400

    fourth

--- AsynsCalls/app/src/main/java/com/example/a1796122/asynscalls/MainActivity.java
+++ AsynsCalls/app/src/main/java/com/example/a1796122/asynscalls/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -21,18 +23,35 @@ public class MainActivity extends AppCompatActivity {
         URL apiURL;
         try{
             apiURL = new URL("https://jsonplaceholder.typicode.com/todos/1");
+            new FetchDataFromAPI().execute(apiURL);
         }
         catch(MalformedURLException e){
             e.printStackTrace();
         }
     }
 
-    public class FetchDataFromAPi extends AsyncTask<URL, Void, String>{
+    public class FetchDataFromAPI extends AsyncTask<URL, Void, String>{
 
         @Override
         protected String doInBackground(URL... urls) {
-            return null;
+            URL myURL = urls[0];
+            String response ="";
+            try {
+                response = NetworkUtility.getResponseFromHttpUrl(myURL);
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+            return response;
         }
+
+        @Override
+        protected void onPostExecute(String s) {
+            TextView tv = findViewById(R.id.tvResult);
+            tv.setText(s);
+        }
+
+        
     }
 
 }
